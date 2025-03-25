143fa5f6f92325f1f7d6773e70e6e9d7f5414e96
Salustiano <salustianomeliann@gmail.com>
2025-03-25 13:47:17 -0300

    Segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 int main (){
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    int x=21, *puntero, tama;
+    puntero = &x;
+    printf("El contenido al que apuntael puntero es: %d\n", *puntero);
+    printf("La direccion de memoria a la que apunta el puntero es: %p \n",*puntero);
+    printf("La direccion de memoria del puntero es: %p\n",&puntero);
+    tama = sizeof(int);
+    printf("El tamaño de la variable es de %d",tama);
     return 0;
 }
